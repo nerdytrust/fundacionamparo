9b63258bcaadb84975e19aa0d1492ee550dfcb53
Eric Bravo <eric.bravo.rod@gmail.com>
2015-04-20 16:13:58 -0500

    Resolviendo problema de botón de navegador de iOS al regresar a la pantalla anterior y vuelva a cargar el efecto de la página

--- maquetacion/js/script.js
+++ maquetacion/js/script.js
@@ -1,13 +1,15 @@
 /***************** Slide-In Nav ******************/
 
-$('.nav_slide_button').click(function() {
-	
+$('.nav_slide_button').click(function(e) {
+	e.preventDefault();
 	if($(this).hasClass("active"))
 		$(this).removeClass("active");
 	else
 		$(this).addClass("active");
 
 	$('.pull').slideToggle();
+	
+	return false;
 });
 	
 
