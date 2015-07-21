6296b1da335192388fc72afbd6f60c4fa5210001
unknown <raggy69@gmail.com>
2015-07-21 14:57:38 -0500

    ajustes timeline css e interior creado

---
+++ app/views/public/muro_exito/interior.blade.php
@@ -0,0 +1,78 @@
+<?php $timeline = 1; $disable_footer = 1; ?>
+@extends( 'public.layout' )
+	@section( 'class' )timeline-section
+	@stop
+	@section( 'content' )
+		<div class="container-fluid">
+		<div class="row">
+		 <section class="slider">
+	        <div class="flexslider interior">
+	          <ul class="slides">
+
+
+	  	    		<li class="col-md-12">
+	  	    	    <img src="../images/tl_becas_estudiantes.jpg" />
+	  	    	    <div class="col-xs-12 col-sm-12 col-md-5 cuadro">
+	  	    	    	<span class="adorno">educación</span>
+	  	    	    	<h1><b>1981</b>
+							Contrucción, operación
+							y administración del CDC,
+							Zacatlan, Puebla.
+						</h1>
+	  	    	    	<h2>
+	  	    	    		Creacion de cntro de desarrollo comunitario Roberto
+							Alonso Espinosa, Ubicados en Zacatlan Puebla.
+	  	    	    	</h2>
+	  	    	    </div>
+	  	    		</li>
+	            	
+
+	  	    		<li class="col-md-12">
+	  	    	    <img src="../images/tl_cartonera.jpg" />
+	  	    	    <div class="col-xs-12 col-sm-12 col-md-5 cuadro">
+	  	    	    	<span class="adorno">educación</span>
+	  	    	    	<h1><b>1998</b>
+							Contrucción, operación
+							y administración del CDC,
+							Zacatlan, Puebla.
+						</h1>
+	  	    	    	<h2>
+	  	    	    		Creacion de cntro de desarrollo comunitario Roberto
+							Alonso Espinosa, Ubicados en Zacatlan Puebla.
+	  	    	    	</h2>
+	  	    	    </div>
+	  	    		</li>
+
+	  	    		<li class="col-md-12">
+	  	    	    <img src="../images/tl_cdcchamontoya.jpg" />
+	  	    	      <div class="col-xs-12 col-sm-12 col-md-5 cuadro">
+	  	    	      	<span class="adorno">educación</span>
+	  	    	    	<h1><b>1997</b>
+							Contrucción, operación
+							y administración del CDC,
+							Zacatlan, Puebla.
+						</h1>
+	  	    	    	<h2>
+	  	    	    		Creacion de cntro de desarrollo comunitario Roberto
+							Alonso Espinosa, Ubicados en Zacatlan Puebla.
+	  	    	    	</h2>
+	  	    	    </div>
+	  	    		</li>
+
+	  	    		
+
+	          </ul>
+
+	          <div class="col-xs-12 col-md-12 controls-ol">
+	          	<button type="button" class="btn ct pull-left"></button>
+	          	<button type="button" class="btn ct2 pull-right"></button>
+	          	<p class="col-md-3">1980</p>
+	          	<p class="col-md-3 pull-right text-right">1980</p>
+	          </div>
+	         
+	        </div>
+	      </section>
+	      </div>
+	</div><!--termina container fluid-->
+	@stop
+@stop
