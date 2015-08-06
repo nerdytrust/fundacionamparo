1cd09f23be79572dfb9122f1fe1bbf1a09da5c19
Eric Bravo <eric.bravo.rod@gmail.com>
2015-08-06 11:26:24 -0500

    Corrección en el query para obtener el total de donadores

--- app/controllers/public/HomeController.php
+++ app/controllers/public/HomeController.php
@@ -67,8 +67,9 @@ class HomeController extends BaseController {
 		$ultimos = array_unique( $ultimos, SORT_REGULAR );
 		$total_donadores = Session::get( 'total_donadores' );
 		if ( ! $total_donadores || empty( $total_donadores ) )
-			$total_donadores = Donaciones::distinct( 'email' )->where( 'status', 1)->count();
-		Session::put( 'total_donadores', $total_donadores );
+			$total_donadores = DB::table('donaciones')->distinct()->where('status', 1)->groupBy('email')->get();
+
+		Session::put( 'total_donadores', count( $total_donadores ) );
 	    return View::make( 'public.home.index' )->with( [ 
 	    	'video' 			=> $video,
 	    	'causas' 			=> $causas,
