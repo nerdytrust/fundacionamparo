1531faf48dd025061630710952a502cfd7c6bb92
mysterio56 <aslanlion56@gmail.com>
2015-08-10 17:59:09 -0500

    add edit_validations array

--- app/models/Videos.php
+++ app/models/Videos.php
@@ -134,6 +134,17 @@ class Videos extends \Crud {
         //
         "validations"     => [],
         //
+        // Validate edit inputs
+        // Rules by column
+        // "email" => "required|min:10|email"
+        // without validation
+        // "imagen" => ""  
+        //
+        // http://laravel.com/docs/4.2/validation#available-validation-rules
+        // 
+        //
+        "edit_validations" => ["cover" => "", "video" => ""],
+        //
         // Columns enable by view
         // Default enable all columns
         //
