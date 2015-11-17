d986c46bbae3e834b2eec9d449f7bf19e55bb1bc
mysterio56 <aslanlion56@gmail.com>
2015-11-17 14:28:20 -0600

    redireccion de gracias en apoyamos tu causa

--- app/views/public/apoyamos/gracias.blade.php
+++ app/views/public/apoyamos/gracias.blade.php
@@ -6,7 +6,7 @@
 		<div class="lightbox-h gracias" id="donar">
 			<div class="lightbox-h-cont donar">
 				<img src="{{ asset( 'images/icon_donadores-v.png' ) }}" alt="">
-				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/' ) }}';"></button>
+				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/apoyamos-tu-causa' ) }}';"></button>
 				<h1>¡gracias!</h1>
 				<h2>Se ha enviado correctamente tu información</h2>
 			</div>
