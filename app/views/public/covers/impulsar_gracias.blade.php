1d137cda5fcfce68ddec535da3ca63769525962f
mysterio56 <aslanlion56@gmail.com>
2015-11-18 10:16:25 -0600

    todas las vistas de gracias regresan a su seccion

--- app/views/public/covers/impulsar_gracias.blade.php
+++ app/views/public/covers/impulsar_gracias.blade.php
@@ -6,7 +6,7 @@
 		<div class="lightbox-h i" id="naranja2">
 			<div class="lightbox-h-cont">
 				<img src="{{ asset( 'images/icon_donadores-v.png' ) }}" alt="">
-				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/' ) }}';"></button>
+				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/impulsar' ) }}';"></button>
 				<!--<button onClick="history.back()" class="regresar"> Regresar</button>-->
 				<h1>¡grácias!</h1>
 				<h2>Ya estás <br/> <b>#TomandoAcciónFA</b></h2>
--- app/views/public/covers/voluntario_gracias.blade.php
+++ app/views/public/covers/voluntario_gracias.blade.php
@@ -6,7 +6,7 @@
 		<div class="lightbox-h" id="naranja2">
 			<div class="lightbox-h-cont">
 				<img src="{{ asset( 'images/icon_donadores-v.png' ) }}" alt="">
-				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/' ) }}';"></button>
+				<button class="cerrar-h" onclick="location.href='{{ URL::to( '/voluntario' ) }}';"></button>
 				<!--<button onClick="history.back()" class="regresar"> Regresar</button>-->
 				<h1>¡gracias!</h1>
 				<h2>Ya estás <br/> <b>#TomandoAcciónFA</b></h2>
