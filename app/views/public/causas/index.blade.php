feff67bf359bdbee2b8a495b57b7c0faed69ca0f
mysterio56 <aslanlion56@gmail.com>
2015-11-19 14:32:57 -0600

    video full causas

--- app/views/public/causas/index.blade.php
+++ app/views/public/causas/index.blade.php
@@ -22,11 +22,11 @@
 								var video = videojs($('.vi3').find('.video-js')[0]).ready(function(){
 								  var player = this;
 								  player.on('ended', function() {
-								  	video.load();
+								  	//video.load();
 								  	$(".vjs-loading-spinner").hide();
 								  });
 								   player.on('error', function() {
-								  	video.load();
+								  	//video.load();
 								  	$(".vjs-loading-spinner").hide();
 								  });
 								});
