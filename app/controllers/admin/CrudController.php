b7bc095b861cf154992e9da66f5d20b9f19e4f87
mysterio56 <aslanlion56@gmail.com>
2015-12-10 13:37:14 -0600

    delete function getEncoding

--- app/controllers/admin/CrudController.php
+++ app/controllers/admin/CrudController.php
@@ -892,24 +892,4 @@ class CrudController extends \BaseController {
                         ]);
     }
 
-    private function getEncoding($rb){ 
-        ## Sustituyo caracteres en la cadena final
-        $rb = str_replace("Ã¡", "&aacute;", $rb);
-        $rb = str_replace("Ã©", "&eacute;", $rb);
-        $rb = str_replace("Â®", "&reg;", $rb);
-        $rb = str_replace("Ã­", "&iacute;", $rb);
-        $rb = str_replace("ï¿½", "&iacute;", $rb);
-        $rb = str_replace("Ã³", "ó", $rb);
-        $rb = str_replace("Ãº", "&uacute;", $rb);
-        $rb = str_replace("n~", "&ntilde;", $rb);
-        $rb = str_replace("Âº", "&ordm;", $rb);
-        $rb = str_replace("Âª", "&ordf;", $rb);
-        $rb = str_replace("ÃƒÂ¡", "&aacute;", $rb);
-        $rb = str_replace("Ã±", "&ntilde;", $rb);
-        $rb = str_replace("Ã‘", "&Ntilde;", $rb);
-        $rb = str_replace("ÃƒÂ±", "&ntilde;", $rb);
-        $rb = str_replace("n~", "&ntilde;", $rb);
-        $rb = str_replace("Ãš", "&Uacute;", $rb);
-        return $rb;
-    }  
 }
