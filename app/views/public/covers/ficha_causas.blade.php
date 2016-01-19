44d44ff2df18642d6a36a9d7b44bdb8a54429205
mysterio56 <aslanlion56@gmail.com>
2016-01-19 14:14:14 -0600

    prueba share title

--- app/views/public/covers/ficha_causas.blade.php
+++ app/views/public/covers/ficha_causas.blade.php
@@ -13,7 +13,7 @@
 					<div id="txt_evento">						
 						<nav>
 							<ul>
-								{{ Helper::facebookShare( '', Request::url().'?v='.str_random(10);, '' ) }}
+								{{ Helper::facebookShare( '', Request::url().'?v='.str_random(10), '' ) }}
 								{{ Helper::twitterShare( $causa->titulo, Request::url(), '' ) }}
 								{{ Helper::like( $causa->id_causas, 'causas' ) }}
 								<p>{{ $causa->me_gusta_interno }} likes</p>
