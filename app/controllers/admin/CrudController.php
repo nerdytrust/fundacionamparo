891c86a0a361f66447eac8dc0c1f3aa50ad71fce
mysterio56 <aslanlion56@gmail.com>
2016-08-04 11:15:08 -0500

    file

--- app/controllers/admin/CrudController.php
+++ app/controllers/admin/CrudController.php
@@ -405,8 +405,10 @@ class CrudController extends \BaseController {
                 
                 foreach ($file_inputs as $file) {
                     if (\Input::file($file)->isValid()) {
-
+                        print_r($file);die;
                       $fileName = md5($class->getTable()."|".$file."|".$params->key_value); // renameing image
+
+                      $fileName =   $columns->{$file}->type
                       \Input::file($file)->move($destinationPath, $fileName); // uploading file to given path
 
                       if($columns->{$file}->type == "text")
