3ab2cf9ee674e6f849373bcd8a5490a79655d289
mysterio56 <aslanlion56@gmail.com>
2016-08-19 16:32:42 -0500

    form

--- app/views/public/covers/donar_step_2.blade.php
+++ app/views/public/covers/donar_step_2.blade.php
@@ -54,22 +54,22 @@
 							
 							<div class="alert alert-danger" role="alert" id="messages"></div>
 							<span class="form-group">
-								{{ Form::text( 'r_nombre', Input::old( 'r_nombre'), [ 'placeholder' => 'Nombre', 'required' => true, 'id' => 'r_nombre', 'class' => 'form-control'] ) }}
+								{{ Form::text( 'r_nombre', Input::old( 'r_nombre'), [ 'placeholder' => 'Nombre', 'id' => 'r_nombre', 'class' => 'form-control'] ) }}
 							</span>
 							
 							<p>RFC</p>
 							<span class="form-group">
-								{{ Form::text( 'r_rfc', Input::old( 'r_rfc' ), [ 'id' => 'r_rfc', 'placeholder' => 'RFC', 'required' => true, 'class' => 'form-control' ] ) }}
+								{{ Form::text( 'r_rfc', Input::old( 'r_rfc' ), [ 'id' => 'r_rfc', 'placeholder' => 'RFC', 'class' => 'form-control' ] ) }}
 							</span>
 
 							<p>Domicilio Fiscal</p>
 							<span class="form-group">
-								{{ Form::text( 'r_domicilio_fiscal', Input::old( 'r_domicilio_fiscal' ), [ 'id' => 'r_domicilio_fiscal', 'placeholder' => 'Domicilio Fisca', 'required' => true, 'class' => 'form-control' ] ) }}
+								{{ Form::text( 'r_domicilio_fiscal', Input::old( 'r_domicilio_fiscal' ), [ 'id' => 'r_domicilio_fiscal', 'placeholder' => 'Domicilio Fisca', 'class' => 'form-control' ] ) }}
 							</span>
 
 							<p>Email</p>
 							<span class="form-group">
-								{{ Form::email( 'r_email', Input::old( 'r_email' ), [ 'id' => 'r_email', 'placeholder' => 'Correo electrónico', 'required' => true, 'class' => 'form-control' ] ) }}
+								{{ Form::email( 'r_email', Input::old( 'r_email' ), [ 'id' => 'r_email', 'placeholder' => 'Correo electrónico', 'class' => 'form-control' ] ) }}
 							</span>
 						
 						</div>
