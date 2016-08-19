3aab021ef32fa75d3980ecda6221052f81862ce7
mysterio56 <aslanlion56@gmail.com>
2016-08-19 12:59:34 -0500

    mail

--- app/controllers/public/DonacionesController.php
+++ app/controllers/public/DonacionesController.php
@@ -757,8 +757,8 @@ class DonacionesController extends BaseController {
 				$session['r_domicilio_fiscal'] = (isset($recibo['r_domicilio_fiscal']))?$recibo['r_domicilio_fiscal']:'';
 				$session['r_email']            = (isset($recibo['r_email']))?$recibo['r_email']:'';
 				$session['r_nombre']           = (isset($recibo['r_nombre']))?$recibo['r_nombre']:'';
-print_r($session);die;
-				$donacionMailFundacion = Mail::send( 'public.mail.donacion_fundacion', ['username' => $nameDonador], function( $message ) use ($session){
+
+				$donacionMailFundacion = Mail::send( 'public.mail.donacion_fundacion', $session, function( $message ) use ($session){
 					$message
 						->from( getenv( 'APP_NOREPLY' ), 'Fundación Amparo' )
 						->to( "aslanlion56@gmail.com", "Fundación Amparo" )
